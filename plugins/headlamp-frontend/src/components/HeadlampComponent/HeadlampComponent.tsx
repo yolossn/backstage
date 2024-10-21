22c97220a34c29559d007e799c9002886fbd2cd0
yolossn <sannagaraj@microsoft.com>
2024-10-22 02:19:19 +0530

    update headlamp-frontend

--- plugins/headlamp-frontend/src/components/HeadlampComponent/HeadlampComponent.tsx
+++ plugins/headlamp-frontend/src/components/HeadlampComponent/HeadlampComponent.tsx
@@ -13,13 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Progress } from '@backstage/core-components';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+interface HeadlampMessage {
+  action: string;
+  redirectPath: string;
+}
 
 export function HeadlampComponent() {
   const [isLoaded, setIsLoaded] = useState(false);
   const refreshInterval = 5000;
   const headlampUrl = 'http://localhost:4466';
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const checkHeadlampReady = async () => {
@@ -44,19 +52,47 @@ export function HeadlampComponent() {
     return undefined;
   }, [isLoaded, headlampUrl]);
 
+  const handleMessage = useCallback(
+    (event: MessageEvent) => {
+      // Ensure the message is from the Headlamp origin
+      if (event.origin !== new URL(headlampUrl).origin) return;
+
+      const data: HeadlampMessage = event.data;
+
+      if (data.redirectPath) {
+        // Navigate to the catalog page for the specific component
+        navigate(data.redirectPath);
+      }
+
+      // Store the received data
+    },
+    [headlampUrl, navigate],
+  );
+
+  useEffect(() => {
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, [handleMessage]);
+
   if (!isLoaded) {
     return <Progress />;
   }
 
+  // Get query parameters
+  const queryParams = new URLSearchParams(location.search).toString();
+  const iframeSrc = queryParams ? `${headlampUrl}?${queryParams}` : headlampUrl;
+
   return (
-    <iframe
-      src={headlampUrl}
-      title="Headlamp"
-      style={{
-        width: '100%',
-        height: '100vh',
-        border: 'none',
-      }}
-    />
+    <>
+      <iframe
+        src={iframeSrc}
+        title="Headlamp"
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+        }}
+      />
+    </>
   );
 }
